d6bdd83a22bc55f325ed69908b1bfb1370289d6a
cklasicki <cklasicki@gmail.com>
2016-05-17 20:44:52 +0200

    dodanie react'a

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,65 @@
 const path = require('path');
 
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         app: PATHS.app
     },
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
+                include: PATHS.app
+
+            }
+        ]
     }
+};
+
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new NpmInstallPlugin({
+                save: true
+            })
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
 }
