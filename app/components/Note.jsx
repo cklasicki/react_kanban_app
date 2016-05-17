6f831adebc0f85b24b5eb9cc414e0bf0581aef81
cklasicki <cklasicki@gmail.com>
2016-05-17 23:00:03 +0200

    w notes dodanie metod edit

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,61 @@
 import React from 'react';
 
-export default ({task, ...props}) => <div {...props}>{task}</div>;
+export default class Note extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editing: false
+        };
+
+        this.renderEdit = this.renderEdit.bind(this);
+        this.renderNote = this.renderNote.bind(this);
+        this.edit = this.edit.bind(this);
+        this.checkEnter = this.checkEnter.bind(this);
+        this.finishEdit = this.finishEdit.bind(this);
+    }
+
+    render() {
+        if (this.state.editing) {
+            return this.renderEdit();
+        }
+        return this.renderNote();
+    }
+
+    renderEdit() {
+        return <input type="text" ref={
+            element => element ? element.selectionStart = this.props.task.length : null
+        }
+            autoFocus={true}
+            defaultValue={this.props.task}
+            onBlur={this.finishEdit}
+            onKeyPress={this.checkEnter} />;
+    }
+
+    renderNote() {
+        return <div onClick={this.edit}>{this.props.task}</div>
+    }
+
+    edit() {
+        this.setState({
+            editing: true
+        });
+    }
+
+    checkEnter(e) {
+        if (e.key === 'Enter') {
+            this.finishEdit(e);
+        }
+    }
+
+    finishEdit(e) {
+        const value = e.target.value;
+
+        if (this.props.onEdit) {
+            this.props.onEdit(value);
+            this.setState({
+                editing: false
+            });
+        }
+    }
+}
